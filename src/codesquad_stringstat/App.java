dbe9be2b09306f363f39157b094c83502a5809a2
wheej <wheej@DESKTOP-K5OAEF1>
2017-07-19 21:56:43 +0900

    -문제 1, 2번 구현

--- src/codesquad_stringstat/App.java
+++ src/codesquad_stringstat/App.java
@@ -1,12 +1,22 @@
 package codesquad_stringstat;
-
+import java.util.HashMap;
 public class App {
 	
 	static final String inputSentence = "Do you know Doctor who?";
 	
 	public static void main (String args[]) {
+		HashMap alphabetCount = new HashMap();
 		
+		String processedSentence = inputSentence.replaceAll("\\?", "");
+		String countSentence = processedSentence.replaceAll(" ", "");
+		String [] words = processedSentence.split(" ");
+		words[0] += "?";
+		for (int i = 0; i < words.length; i++) {
+			
+			System.out.print(words[words.length - i - 1] + " ");
+		}
 		
+		System.out.println("\n전체 수: " + countSentence.length());
 	}
 
 }
