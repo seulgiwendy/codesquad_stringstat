bb3a3e4421666b4a58c62600831a0f83933ab727
wheej <wheej@DESKTOP-K5OAEF1>
2017-07-19 21:29:43 +0900

    -initial commit

---
+++ src/codesquad_stringstat/App.java
@@ -0,0 +1,12 @@
+package codesquad_stringstat;
+
+public class App {
+	
+	static final String inputSentence = "Do you know Doctor who?";
+	
+	public static void main (String args[]) {
+		
+		
+	}
+
+}
